da3eff3929a60a79901985b060622e8e8132dcf5
jiwoo1304 <dayjimin0811@gmail.com>
2024-08-26 20:50:14 +0900

    fect:dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import DropdownMenu from "@/components/dropdownMenu";
-import Button from "../components/page";
-import { JiButton } from "@l0ylz/l0ylz-next-ui-kit";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex justify-center w-full items-center h-screen">
-        <div style={{ border: "solid 1px red" }}>
-          <JiButton paddingX="12px" bgColor="dark">
-            click
-          </JiButton>{" "}
-          <div>
-            <DropdownMenu />
-          </div>
+      <div className="w-full h-screen flex justify-center items-center bg-white text-zinc-800">
+        <div className="flex gap-6">
+          {headerNav.map((item, index) => (
+            <DropdownMenu menu={menus} key={index}>
+              <div style={{ paddingInline: "10px" }}>{item}</div>
+            </DropdownMenu>
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+const headerNav = ["User"];
+
+const menus = [
+  { title: "menu 1", icon: <div>.</div> },
+  {},
+  { title: "menu 2" },
+];
--- components/dropdownMenu.tsx
+++ components/dropdownMenu.tsx
@@ -1,36 +1,92 @@
 "use client";
-import { title } from "process";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 
-interface Props {}
+const alignType = {
+  left: 0,
+  right: "",
+};
 
-const DropdownMenu: React.FC<Props> = ({}) => {
-  const [ShowMenu, setShowMenu] = useState(false);
+interface Props {
+  children: ReactNode;
+  menu: { title?: string; icon?: JSX.Element }[];
+  align?: "left" | "right";
+  textColor?: string;
+  backgrounColor?: string;
+}
 
-  if (!ShowMenu) {
-    return null;
-  }
+const DropdownMenu: React.FC<Props> = ({
+  children,
+  menu,
+  align,
+  textColor,
+  backgrounColor,
+}) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const closeHandle = () => {
+    setShowMenu(false);
+  };
 
   return (
-    <div
-      style={{
-        backgroundColor: "white",
-        color: "#331",
-        position: "absolute",
-        minWidth: "6rem",
-        padding: "4px",
-      }}
-    >
-      {menus.map((item, index) => (
-        <div key={index} className="flex">
-          <div>{item.title}</div>
-          <div>{item.icon}</div>
+    <div>
+      {/* 메뉴를 제외한 전체화면 클릭시 사라짐 */}
+      {showMenu && (
+        <div
+          onClick={closeHandle}
+          style={{
+            zIndex: 10,
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        />
+      )}
+
+      {/* 상단메뉴 */}
+      <div
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+      >
+        {children}
+        <div style={{ position: "relative", border: "solid 1px red" }}>
+          {showMenu && (
+            <div
+              style={{
+                position: "absolute",
+                left: align ? alignType[align] : "",
+                right: 0,
+                top: 0,
+                backgroundColor: backgrounColor ? backgrounColor : "",
+                color: textColor ? textColor : "",
+                minWidth: "6rem",
+                padding: "4px",
+                zIndex: "10",
+                boxShadow: "2px 2px 6px rgba(100, 100, .2)",
+              }}
+            >
+              {menu.map((item, index) =>
+                item.icon || item.title ? (
+                  <div
+                    onClick={closeHandle}
+                    key={index}
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <div>{item.icon}</div>
+                    <div>{item.title}</div>
+                  </div>
+                ) : (
+                  <div key={index} style={{ border: "solid 0.5px " }}></div>
+                )
+              )}
+            </div>
+          )}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
 
 export default DropdownMenu;
-
-const menus = [{ title: "menu 1" }, { icon: "menu 2" }];
